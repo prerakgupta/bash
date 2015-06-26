fa8eeaa96149f13bff5b9bc331cff23dad84af07
Prerak Gupta <prerakgupta@Preraks-MacBook-Pro.local>
2015-06-26 20:32:38 +0530

    Updated script

--- script1.sh
+++ script1.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 clear
-time=0ms
-var=$(grep Completed tmp_more_than_50 | awk '{ if($2==200){ printf $5;} else{print $7;}print " "; }');
+var=$(grep Completed tmp_more_than_50 | awk '{ if($2==200){ printf $5;} else{print $7;}print " "; }' | sort -r -n);
 arrTIME=(${var})
-for i in "${!arrTIME[@]}"; do
-    if [[ "$time" < "${arrTIME[i]}" ]]; then time="${arrTIME[i]}"; fi;
-done
-echo "Maximum process time is $time"
-grep "$time" tmp_more_than_50 | awk '{print $1, $2, $3, $4, $5;}'
+max=("${arrTIME[@]:0:1}");
+grep "$max" tmp_more_than_50 | awk '{print $1, $2, $3, $4, $5;}'
