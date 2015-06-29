b840f4376ff22b4491ac964c8202497334c5d195
prerakgupta <prerak@browserstack.com>
2015-06-29 14:36:21 +0530

    Update script1.sh
    It now prints the request too.

--- script1.sh
+++ script1.sh
@@ -3,4 +3,4 @@ clear
 var=$(grep Completed tmp_more_than_50 | awk '{ if($2==200){ printf $5;} else{print $7;}print " "; }' | sort -r -n);
 arrTIME=(${var})
 max=("${arrTIME[@]:0:1}");
-grep "$max" tmp_more_than_50 | awk '{print $1, $2, $3, $4, $5;}'
+grep -B 1 "$max" tmp_more_than_50 
